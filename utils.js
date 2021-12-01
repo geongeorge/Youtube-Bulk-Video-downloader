f6b8139507971fa31c134d13893d3b2125eea4f3
Thales Menezes <thlmenezes@pm.me>
2021-12-01 11:15:29 -0300

    update comment, give credit to source

--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 const isArray = (arr) => Array.isArray(arr) && arr instanceof Array && Boolean(arr.length)
 
+// NOTE: Flatten method based on [source](https://thewebdev.info/2021/04/18/how-to-flatten-a-nested-json-object-with-javascript/)
 const flatten = (obj, prefix = [], current = {}) => {
   if (typeof(obj) === 'object' && obj !== null) {
     if(isArray(obj)){
